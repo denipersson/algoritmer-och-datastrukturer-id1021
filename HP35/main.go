2e3c322946f743e6c383e890e0b289a75a0ca428
Deni Persson <deni.persson@gmail.com>
2022-09-02 00:44:57 +0200

    Update main.go

--- HP35/main.go
+++ HP35/main.go
@@ -12,7 +12,7 @@ func test_static_calc() {
 
 	items := []Item{New_Item(VALUE, 10), New_Item(VALUE, 5), New_Item(SUB, 0),
 		New_Item(VALUE, 4), New_Item(ADD, 0), New_Item(VALUE, 3), New_Item(DIV, 0)}
-	//expected result: (10 - 5 + 4)/3  = (5+4)/3 = 9/3 = 3
+	//Expected result: (10 - 5 + 4)/3  = (5+4)/3 = 9/3 = 3
 
 	stack := New_Stack(4, false)
 
