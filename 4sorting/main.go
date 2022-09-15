62fe3bc1ed9e3f1a47220e3cf4f331bb9fbb1c61
Deni Persson <deni.persson@gmail.com>
2022-09-16 00:19:33 +0200

    m

--- 4sorting/main.go
+++ 4sorting/main.go
@@ -9,22 +9,39 @@ import (
 func main() {
 	rand.Seed(time.Now().UnixMilli())
 
-	for i := 0; i < 1000; i++ {
-		//test_section_sort(i)
+	for i := 0; i <= 10000; i += 1000 {
 		test_insertion_sort(i)
 	}
+
+	/*
+		test_merge_sort(2)
+		test_merge_sort(4)
+		test_merge_sort(8)
+		test_merge_sort(16)
+		test_merge_sort(32)
+		test_merge_sort(64)
+		test_merge_sort(128)
+		test_merge_sort(256)
+		test_merge_sort(512)
+		test_merge_sort(1024)
+		test_merge_sort(2048)
+		test_merge_sort(4096)
+		test_merge_sort(8192)
+		test_merge_sort(16384)
+		test_merge_sort(16384 * 2)
+		test_merge_sort(16384 * 2 * 2)
+		test_merge_sort(16384 * 2 * 2 * 2)
+		test_merge_sort(16384 * 2 * 2 * 2 * 2)
+	*/
 }
 func test_selection_sort(mult int) {
 	size := 10 * mult
 
 	arr := rand_int_arr(size)
-	//fmt.Println("Unsorted: ", arr)
-	/*sorted_arr := */
 	t0 := time.Now()
 	selection_sort(arr)
 	t_after := time.Since(t0)
 	fmt.Println(len(arr), " ", t_after.Microseconds())
-	//fmt.Println("Sorted(section sort): ", sorted_arr)
 }
 
 func rand_int_arr(size int) []int {
@@ -57,11 +74,10 @@ func selection_sort(arr []int) []int {
 func insertion_sort(arr []int) []int {
 	for i := 1; i < len(arr); i++ {
 		j := i
-
-		for j > 0 {
-			if arr[j-1] > arr[j] {
-				arr[j-1], arr[j] = arr[j], arr[j-1]
-			}
+		for j > 0 && arr[j-1] > arr[j] {
+			copy := arr[j]
+			arr[j] = arr[j-1]
+			arr[j-1] = copy
 			j--
 		}
 	}
@@ -72,23 +88,60 @@ func test_insertion_sort(mult int) {
 	size := 10 * mult
 
 	arr := rand_int_arr(size)
-	//fmt.Println("Unsorted: ", arr)
 	t0 := time.Now()
-	/*sorted_arr := */ insertion_sort(arr)
+	insertion_sort(arr)
 	t_after := time.Since(t0)
 	fmt.Println(len(arr), " ", t_after.Microseconds())
-	//fmt.Println("Sorted(insertion sort): ", sorted_arr)
+
 }
 
-/*
+func test_merge_sort(mult int) {
+	size := 10 * mult
 
-	for (int i = 0; i < array.length -1; i++) {
-		// let's set the first candidate to the index itself int cand = i;
-		for (int j = i; j < array.length ; j++) {
-			// If the element at position j is smaller than the value
-			// at the candidate position - then you have a new candidate
-			// posistion.
-		}
-		// Swap the item at position i with the item at the candidate position.
+	arr := rand_int_arr(size)
+	t0 := time.Now()
+	merge_sort(arr)
+	t_after := time.Since(t0)
+	fmt.Println(len(arr), " ", t_after.Microseconds())
+}
+
+func merge_sort(arr []int) []int {
+
+	if len(arr) == 1 {
+		return arr
 	}
-*/
+
+	arr1 := arr[:(len(arr))/2]
+	arr2 := arr[(len(arr))/2:]
+
+	arr1 = merge_sort(arr1)
+	arr2 = merge_sort(arr2)
+
+	return merge(arr1, arr2)
+}
+func merge(arra []int, arrb []int) []int {
+
+	last := make([]int, len(arra)+len(arrb))
+	a_index, b_index, merge_index := 0, 0, 0
+
+	for ; a_index < len(arra) && b_index < len(arrb); merge_index++ {
+		if arra[a_index] < arrb[b_index] {
+			last[merge_index] = arra[a_index]
+			a_index++
+		} else {
+			last[merge_index] = arrb[b_index]
+			b_index++
+		}
+	}
+	for len(arra) > a_index {
+		last[merge_index] = arra[a_index]
+		a_index++
+		merge_index++
+	}
+	for len(arrb) > b_index {
+		last[merge_index] = arrb[b_index]
+		b_index++
+		merge_index++
+	}
+	return last
+}
